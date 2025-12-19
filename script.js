fd197ac6036605cd4ecba687baa286eac2263114
alazbaalaa <147321480+alazbaalaa@users.noreply.github.com>
2025-12-19 13:39:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+document.querySelectorAll('section, .skill-card, .project-card')
+    .forEach(el => {
+        el.classList.add('hidden');
+        observer.observe(el);
+    });
